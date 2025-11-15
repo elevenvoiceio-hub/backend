4fa5081fc50100837ee9055f5c83af5bdb6feae5
mohitmadras <mohitmadras@gmail.com>
2025-11-15 18:44:11 +0530

    updated delete api

--- voice_cloning/urls.py
+++ voice_cloning/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import (
     VoiceCloneCreateAPIView,
+    VoiceClonesDeleteAPIView,
     VoiceClonesListAPIView,
 )
 
@@ -13,7 +14,7 @@ urlpatterns = [
     path("clones/", VoiceClonesListAPIView.as_view(), name="voiceClones-list"),
     path(
         "clones/delete/<str:clone_id>/",
-        VoiceClonesListAPIView.as_view(),
+        VoiceClonesDeleteAPIView.as_view(),
         name="voiceclone-delete-custom",
     ),
 ]
